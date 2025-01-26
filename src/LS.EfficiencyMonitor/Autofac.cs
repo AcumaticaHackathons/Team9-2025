5bba4ad845171be8625b46ff8e2a1f0ad96cc6e8
kyle <kyle@contou.com>
2025-01-25 19:04:05 -0700

    Remove Usings

--- src/LS.EfficiencyMonitor/Autofac.cs
+++ src/LS.EfficiencyMonitor/Autofac.cs
@@ -1,12 +1,5 @@
 ﻿using Autofac;
-using Autofac.Core;
-using Autofac.Core.Registration;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LS.EfficiencyMonitor
 {
