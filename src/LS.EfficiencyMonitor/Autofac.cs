eedbbe458c8a19c4823f5c092afd4c5f1c4e5b34
kyle <kyle@contou.com>
2025-01-25 19:03:50 -0700

    Background Service

---
+++ src/LS.EfficiencyMonitor/Autofac.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Autofac.Core;
+using Autofac.Core.Registration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LS.EfficiencyMonitor
+{
+    public class EfficiencyAutofac  : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // Register MyHostedService as IHostedService
+            builder.RegisterType<CpuUtilizationMonitorService>()
+                   .As<IHostedService>()
+                   .SingleInstance();  // This makes it singleton, adjust as needed
+        }
+    }
+}
---
+++ src/LS.EfficiencyMonitor/CpuUtilizationMonitor.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Hosting;
+using PX.SM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using PX.Data;
+
+namespace LS.EfficiencyMonitor
+{
+    public class CpuUtilizationMonitorService : IHostedService
+    {
+        private Timer _monitoringTimer;
+        private readonly List<int> _cpuUtilizationHistory = new(MaxHistorySize);
+        private const int MaxHistorySize = 3600;
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _monitoringTimer = new Timer(GetUtilization, _cpuUtilizationHistory,2000,2000);
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _monitoringTimer.Dispose();
+        }
+
+        public void GetUtilization(object state)
+        {
+            List<int> history = (List<int>)state;
+            // Get the type of PXPerformanceMonitor
+            Type type = typeof(PXPerformanceMonitor);
+
+            // Get the internal static property 'CPUUtilization' using reflection
+            PropertyInfo propertyInfo = type.GetProperty("CPUUtilization", BindingFlags.NonPublic | BindingFlags.Static);
+
+            if (propertyInfo != null)
+            {
+                // Get the value of the property
+                object value = propertyInfo.GetValue(null); // For static properties, pass null for the object parameter
+
+                // Assuming the property is of type int, cast the value to int
+                int cpuUtilization = (int)value;
+                if (history.Count == MaxHistorySize)
+                {
+                    history.RemoveAt(0);
+                }
+                history.Add(cpuUtilization);
+            }
+            else
+            {
+                throw new Exception("Property 'CPUUtilization' not found.");
+            }
+        }
+    }
+}
